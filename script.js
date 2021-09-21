1eddf6fb449673e63bd2a1acbf3c159031266a2c
vanessatrentin <vanessa.trentin@gmail.com>
2021-09-20 21:36:17 -0300

    doing a calculator feature as a js class

--- script.js
+++ script.js
@@ -1,99 +1,51 @@
-const allClear = document.getElementById('allClear')
-const clearContent = document.getElementById('clearContent')
-
-const multiplication = document.getElementById('multiplication')
-const division = document.getElementById('division')
-const subtraction = document.getElementById('subtraction')
-const addiction = document.getElementById('addiction')
-
-const number0 = document.getElementById('number0')
-const number1 = document.getElementById('number1')
-const numberTwo = document.getElementById('numberTwo')
-const number3 = document.getElementById('number3')
-const number4 = document.getElementById('number4')
-const number5 = document.getElementById('number5')
-const number6 = document.getElementById('number6')
-const number7 = document.getElementById('number7')
-const number8 = document.getElementById('number8')
-const number9 = document.getElementById('number9')
-
-const fraction = document.getElementById('fraction')
-
-const result = document.getElementById('result')
-
-let calculatorEntry = document.getElementById('calculatorEntries')
-let showEquation = document.getElementById('showEquation')
-
-let calculation = []
-let equation = []
-let operation = false
-
-number1.addEventListener('click', toType1)
-
-numberTwo.addEventListener('click', toType2)
-
-result.addEventListener('click', resolution)
-
-
-
-function toType2() {
-    if(calculatorEntry.value == 0){
-        calculatorEntry.value = 2
-    } else if(operation == true){
-        toClearAll()
-        operation = false
-        calculatorEntry.value = 2
-    } 
-    else{
-        calculatorEntry.value = calculatorEntry.value + '2'
+class Calculator {
+    constructor(priorOperandText, currentOperandText){
+        this.priorOperandText = priorOperandText
+        this.currentOperandText = priorOperandText
+        this.clear()
     }
-}  
 
+    clear() {
+        this.currentOperand = ''
+        this.priorOperand = ''
+        this.calculation = undefined
+    }
 
-function toType1() {
-    if(calculatorEntry.value == 0){
-        calculatorEntry.value = 1
-    } else if(operation == true){
-        toClearAll()
-        operation = false
-        calculatorEntry.value = 1
-    } else {
-        calculatorEntry.value = calculatorEntry.value + '1'
+    delete() {
+
+    }
+
+    addNumber(number) {
+        this.currentOperand = number
+    }
+
+    chooseOperation(Operand) {
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay() {
+        this.currentOperandText.innerText = this.currentOperand
     }
 
 }
 
-allClear.addEventListener('click', toClearAll)
-/*clearContent.addEventListener('click', toClearLastContent) */
-multiplication.addEventListener('click', toMultiplicate)
+const numberButtons = document.querySelectorAll('[data-number]')
+const OperandButtons = document.querySelectorAll('[data-operand]')
+const resultButton = document.querySelector('[data-result]')
+const deleteButton = document.querySelector('[data-delete')
+const allClearButton = document.querySelector('[data-all-clear]')
+const priorOperandText = document.querySelector('[data-prior-operand]')
+const currentOperandText = document.querySelector('[data-current-operand]')
 
-function toClearAll() {
-    calculatorEntry.value = 0
-    console.log('everything is clear')
-}
+const calculator = new Calculator(priorOperandText, currentOperandText)
 
-function toClearLastContent() {
-    console.log('last digit is clear')
-}
-
-function toMultiplicate() {
-    operation = true
-    pushInputEntry()
-    calculation.push('*')
-}
-
-function pushInputEntry() {
-    calculation.push(calculatorEntry.value)
-    console.log(calculation)
-}
-
-function resolution() {
-    for(i = 0; i < calculation.length; i++){
-        console.log(calculation[i])
-
-    }
-}
-
-
-
-
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.addNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
